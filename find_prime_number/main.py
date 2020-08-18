cfe41d2dd0c27f8ba8f67f73e86fd4cfac37d187
theballmarcus <mcihme@gmail.com>
2020-08-18 20:26:45 +0200

    Prime numbers solution 1

---
+++ find_prime_number/main.py
@@ -0,0 +1,20 @@
+# 
+#              Program written by: Marcus, cloned from rep: https://github.com/theballmarcus/math_solutions
+#              Feel free to use this code, but I would appreciate keeping the repo link in the top of the file.
+#
+
+
+cur_number = 1
+stopAt = 1000000
+
+for x in range(stopAt):
+    flag = True
+    cur_number = cur_number + 2
+    
+    for i in range(2, (cur_number // 2) + 1):
+        if(cur_number % i == 0):
+            flag = False
+            break
+        
+    if(flag == True):
+        print('Prime found: %d' %cur_number)
