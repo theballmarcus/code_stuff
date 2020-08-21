01a61ad0f4ef01d95402922042afc651e4508a56
Testerdasd <testerdasd@gmail.com>
2020-08-21 10:23:47 +0200

    Demo tripple shops beach location initialize

---
+++ tripple_shops_beach_locations/main.py
@@ -0,0 +1,12 @@
+'''
+Program written by: Elliot, Marcus
+Code copied from GitHub Repo:  https://github.com/theballmarcus/math_solutions.git
+
+'''
+#IMPORT SECTION
+
+
+#DEFINEMENT SECTION
+
+
+#MAIN PROGRAM
