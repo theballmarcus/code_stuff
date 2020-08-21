38099b7f589d6dea92ca4588376ad720a786aa27
Testerdasd <testerdasd@gmail.com>
2020-08-21 12:38:04 +0200

    First tripple_shop_locations demo

--- tripple_shops_beach_locations/main.py
+++ tripple_shops_beach_locations/main.py
@@ -7,6 +7,34 @@ Code copied from GitHub Repo:  https://github.com/theballmarcus/math_solutions.g
 
 
 #DEFINEMENT SECTION
+dicti = [
+    {"a" : {}},
+    {"b" : {}}, 
+    {"c" : {}}
+    ]
 
+
+x = None
+y = None
+z = None
 
 #MAIN PROGRAM
+
+
+for a in range(0,100):
+    for b in range(0,100):
+        if a == b:
+            pass
+        else:
+            for c in range(0,100):
+                if a == c or b == c:
+                    pass
+                else:
+                    curVal = [
+                        {"a" : a}, 
+                        {"b" : b}, 
+                        {"c" : c}
+                        ]
+                    print(curVal)
+                    curBest = {"a" : None, "b" : None, "c" : None}
+                    
